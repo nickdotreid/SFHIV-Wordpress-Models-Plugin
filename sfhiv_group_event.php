a06f6bd545525b7091a53952f3b1eebb45986c91
Nick Reid <nickreid@nickreid.com>
2012-07-11 14:18:31 -0700

    changed meeting titles

--- sfhiv_group_event.php
+++ sfhiv_group_event.php
@@ -35,5 +35,31 @@ function sfhiv_group_event_pass_status($event_id){
 	add_action('save_post','sfhiv_group_event_inherit_status');
 }
 
+function sfhiv_group_event_update_title($title, $post_ID){
+	if(get_post_type($post_ID) != 'sfhiv_event') return $title;
+	$groups = new WP_Query(array(
+		'connected_type' => 'group_events',
+		'connected_items' => $post_ID,
+		'nopaging' => true,
+		'post_type' => 'sfhiv_group',
+	));
+	$title = "";
+	for($i=0;$i<count($groups->posts);$i++){
+		$group = $groups->posts[$i];
+		$group_title = apply_filters('the_title',$group->post_title,$group->ID);
+		if($i>0){
+			$connection = ", ";
+			if($i==count($group->posts)-1) $connection = ", and ";
+			$title = $title.$connection.$group_title;
+		}else{
+			$title = $group_title;
+		}
+	}
+	$title = $title." Meeting";
+	return $title;
+}
+add_filter('single_post_title', 'sfhiv_group_event_update_title', 10, 2);
+add_filter('the_title', 'sfhiv_group_event_update_title', 10, 2);
+
 
 ?>
