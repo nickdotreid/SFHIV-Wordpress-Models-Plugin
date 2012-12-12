fe10e1484405bd8244e8dbbd3a6779e47efa8755
Nick Reid <nickreid@nickreid.com>
2012-12-12 14:04:53 -0800

    made website link use custom meta box

--- sfhiv_website_link.php
+++ sfhiv_website_link.php
@@ -1,55 +1,43 @@
 <?php
 
-add_action('add_meta_boxes','sfhiv_website_link_add_all_boxes');
-function sfhiv_website_link_add_all_boxes(){
-	sfhiv_website_link_add_meta_box('sfhiv_document');
-	sfhiv_website_link_add_meta_box('sfhiv_study');
-	sfhiv_website_link_add_meta_box('sfhiv_group');
-	sfhiv_website_link_add_meta_box('sfhiv_service');
-	sfhiv_website_link_add_meta_box('sfhiv_service_hour');
-	sfhiv_website_link_add_meta_box('sfhiv_event');
-	sfhiv_website_link_add_meta_box('post');
-	sfhiv_website_link_add_meta_box('page');
+add_filter( 'cmb_meta_boxes', 'sfhiv_website_link_box', 10 );
+function sfhiv_website_link_box( $meta_boxes ){
+	$sfhiv_website_linked_post_types = array(
+		'sfhiv_document',
+		'sfhiv_study',
+		'sfhiv_group',
+		'sfhiv_service',
+		'sfhiv_service_hour',
+		'sfhiv_service_provider',
+		'sfhiv_event',
+		'post',
+		'page',
+		);
+	$meta_boxes[] = array(
+		'id'         => 'sfhiv_website_link',
+		'title'      => 'External Link',
+		'pages'      => $sfhiv_website_linked_post_types,
+		'context'    => 'side',
+		'priority'   => 'high',
+		'show_names' => true, // Show field names on the left
+		'fields' => array(
+			array(
+				'name' => 'External Link',
+				'desc' => 'External link for this content item',
+				'id'   => 'sfhiv_website_link',
+				'type' => 'text',
+			),
+			array(
+				'name' => 'Forward',
+				'desc' => 'Automatically Forward All Traffic',
+				'id'   => 'sfhiv_website_link_forward',
+				'type' => 'checkbox',
+			),
+		)
+	);
+	return $meta_boxes;
 }
 
-function sfhiv_website_link_add_meta_box($post_type){
-	add_meta_box( 'sfhiv_website_link', 'Website', 'sfhiv_website_link_meta_box', $post_type);
-}
-
-function sfhiv_website_link_meta_box($post){
-	$link = get_post_meta($post->ID,'sfhiv_website_link',true);
-	$name = get_post_meta($post->ID,'sfhiv_website_link_name',true);
-	$forward = get_post_meta($post->ID,'sfhiv_website_link_forward',true);
-	?>
-	<div>
-		<label for="sfhiv_website_link">Website</label>
-		<input type="text" id="sfhiv_website_link" name="sfhiv_website_link" value="<?=$link;?>" />
-		<label class="checkbox"><input type="checkbox" name="sfhiv_website_link_forward" value="true" <? if($forward) echo 'checked="checked"';	?> />Automatically Forward</label>
-	</div>
-	<div>
-		<label for="sfhiv_website_link_name">Name</label>
-		<input type="text" id="sfhiv_website_link_name" name="sfhiv_website_link_name" value="<?=$name;?>" />
-	</div>
-	<?
-}
-
-add_action('save_post','sfhiv_website_link_save');
-function sfhiv_website_link_save($post_ID){
-	if($_POST['ID'] != $post_ID) return;
-	if(isset($_POST['sfhiv_website_link']) && $_POST['sfhiv_website_link'] != ""){
-		update_post_meta($post_ID,'sfhiv_website_link',$_POST['sfhiv_website_link']);
-		update_post_meta($post_ID,'sfhiv_website_link_name',$_POST['sfhiv_website_link_name']);
-		if(isset($_POST['sfhiv_website_link_forward'])){
-			update_post_meta($post_ID,'sfhiv_website_link_forward',$_POST['sfhiv_website_link_forward']);
-		}else{
-			delete_post_meta($post_ID,'sfhiv_website_link_forward');
-		}
-	}else if(isset($_POST['sfhiv_website_link'])){
-		delete_post_meta($post_ID,'sfhiv_website_link');
-		delete_post_meta($post_ID,'sfhiv_website_name');
-		delete_post_meta($post_ID,'sfhiv_website_link_forward');
-	}
-}
 
 function sfhiv_website_link_get_link($post_ID=false){
 	if(!$post_ID) $post_ID = get_the_ID();
